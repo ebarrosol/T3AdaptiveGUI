3abd48604b27e6fa1b45eb74193ee5b549b2fc25
efrenalejandrobll <efrenalejandro@efrenalejandroblls-Mac.local>
2015-05-28 08:08:08 -0700

    Initial Commit

---
+++ T3AdaptiveGUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T3AdaptiveGUI
+//
+//  Created by 	efrenalejandrobll on 5/28/15.
+//  Copyright (c) 2015 	efrenalejandrobll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
